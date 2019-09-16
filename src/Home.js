5f6379ebca291ffded7257d1390b7dbfbaec825e
System Administrator <root@news-MacBook-Air.local>
2019-09-16 19:18:59 +0100

    homepage again

--- src/Home.js
+++ src/Home.js
@@ -14,7 +14,9 @@ const Home = () => {
   <Navigation />
   <div>
     <Logo />
-    <p  style = {{display: 'flex', justifyContent: 'center'}} className = "pt2 pb3 f4 f3-ns ml1 ml7-ns mr1 mr7-ns"> Her Bra&ensp;&ensp;•&ensp;&ensp;Her Self-Esteem&ensp;&ensp;•&ensp;&ensp;Her Period</p><br></br><br></br>
+    <div style = {{display: 'flex', justifyContent: 'center', width: 'fit-content', margin: '0 auto'}} ><p
+    className = "pa2 pa3-ns f4 f3-ns ml1 ml7-ns mr1 mr7-ns"> 
+    Her Bra&ensp;&ensp;•&ensp;&ensp;Her Self-Esteem&ensp;&ensp;•&ensp;&ensp;Her Period</p></div><br></br><br></br>
     <Gold/>
 </div>
 </div>
